ec9d7679aae9d98215304eac157130a7e63475b3
tiago.paranhos <tiagoprn@gmail.com>
2020-10-01 07:10:32 -0300

    style: linter fixes.

--- personal_api/personal_api/urls.py
+++ personal_api/personal_api/urls.py
@@ -1,11 +1,12 @@
-from core import views as core_views
 from django.contrib import admin
 from django.urls import path, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from personal_api import views as project_views
 from rest_framework import permissions
 from rest_framework_simplejwt import views as jwt_views
+
+from core import views as core_views
+from personal_api import views as project_views
 
 schema_view = get_schema_view(
     openapi.Info(title='Swagger API', default_version='v1'),
