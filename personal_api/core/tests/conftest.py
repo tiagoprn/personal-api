11ad7c8c022926fa09016be50fe2cb5703c14c86
tiago.paranhos <tiagoprn@gmail.com>
2020-10-21 07:55:14 -0300

    test: refactoring user authentication tests and fixtures.

--- personal_api/core/tests/conftest.py
+++ personal_api/core/tests/conftest.py
@@ -1,5 +1,7 @@
 # Here go your pytest fixtures, that can be used on your tests.
 import pytest
+from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from core.models import CustomUser
 from core.tests.factories import UserModelFactory
@@ -27,3 +29,16 @@ def real_user():
     user.set_password('12345678')
     user.save()
     return user
+
+
+@pytest.fixture()
+def authenticated_api_client(real_user):
+    client = APIClient()
+    refresh = RefreshToken.for_user(real_user)
+    client.credentials(HTTP_AUTHORIZATION=f'Bearer {refresh.access_token}')
+    return client
+
+
+@pytest.fixture()
+def anonymous_api_client():
+    return APIClient()
--- personal_api/core/tests/test_user.py
+++ personal_api/core/tests/test_user.py
@@ -1,27 +1,23 @@
 import pytest
-from django.test import Client
-from rest_framework.test import APIClient
-
-client = Client()
 
 
 @pytest.mark.django_db
-def test_access_token_on_greetings_authenticated_endpoint(real_user):
-    # token generation
-    credential_response = client.post(
+def test_access_token_generation_must_pass_on_existing_user(
+    real_user, anonymous_api_client
+):
+    credential_response = anonymous_api_client.post(
         '/api/token/', {'username': real_user.username, 'password': '12345678'}
     )
     assert credential_response.status_code == 200
-
-    credential_response_as_json = credential_response.json()
     assert {'refresh', 'access'}.intersection(
-        set(credential_response_as_json.keys())
+        set(credential_response.json().keys())
     )
 
-    # use token at authenticated greetings endpoint
-    # curl http://localhost:8000/core/greetings/ -H "Authorization: Bearer $(token)"
-    token = credential_response_as_json['access']
-    api_client = APIClient()
-    api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
-    greetings_authenticated_response = api_client.get('/core/greetings/')
-    assert greetings_authenticated_response.status_code == 200
+
+@pytest.mark.django_db
+def test_access_to_authenticated_enpoint_must_pass_on_existing_user(
+    real_user, authenticated_api_client
+):
+    response = authenticated_api_client.get('/core/greetings/')
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Hello there, hal_jordan!'}
