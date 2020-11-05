35bcffcd4a55df0e6ee31837da9c29abcb67f351
tiago.paranhos <tiagoprn@gmail.com>
2020-11-05 07:34:44 -0300

    test: (WIP) bootstraping the model tests to validate manager custom filters.

---
+++ personal_api/personal_api/tests/test_models.py
@@ -0,0 +1,26 @@
+from core.models import Url
+
+
+"""
+IMPORTANT: use real model instances in these tests, not the model factory -
+so to the ensure the models as working properly
+"""
+
+
+def test_most_recent_filter():
+    # TODO
+    # most_recent_urls = Url.objects.most_recent(days=1)
+    pass
+
+
+def test_from_user_filter():
+    # TODO
+    # user_urls = Url.objects.from_user(user=user)
+    pass
+
+
+def test_most_recent_and_from_user_filters_together():
+    # TODO
+    # user_most_recent_urls = Url.objects.from_user(
+    #     user=user).most_recent(days=1)
+    pass
