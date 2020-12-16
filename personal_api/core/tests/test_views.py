a03a156ae6993db97580cc97d6e1f0b780ad9035
tiago.paranhos <tiagoprn@gmail.com>
2020-12-16 06:57:58 -0300

    test: better organization of the views' tests.

--- personal_api/core/tests/test_views.py
+++ personal_api/core/tests/test_views.py
@@ -1,51 +1,21 @@
-import factory
 import pytest
-from faker import Faker
-from rest_framework.test import APIClient
-
-from core.tests.factories import UrlModelFactory, UserModelFactory
 
 
 @pytest.mark.django_db
-class TestViewsSample:
-    def setup(self):
-        self.client = APIClient()
-
-    @pytest.mark.skip('REFACTOR')
-    def test_using_pytest_fixture_and_factory(self):
-        # TODO: change the test below to NOT use the factory, due to
-        #       urls must exists for it to pass. Or add a way for it to
-        #       feed the url with existing ones.
-        urls = set()
-        while len(urls) < 60:
-            fake_url = Faker().uri()
-            if len(fake_url) < 200:
-                urls.add(fake_url)
-
-        user = UserModelFactory()
-
-        UrlModelFactory.create_batch(
-            60,
-            user=user,
-            name=factory.Sequence(lambda n: n + 1),
-            original_url=factory.Iterator(urls),
-        )
-        assert True
+def test_access_to_greetings_endpoint_must_pass_on_existing_user(
+    real_user, authenticated_api_client
+):  # pylint: disable=unused-argument
+    response = authenticated_api_client.get('/core/greetings/')
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Hello there, hal_jordan!'}
 
 
-@pytest.mark.integration
-class TestViewsIntegration:
-    def test_integration(self):
-        """
-        This is an integration test that depends on external databases,
-        slow services, etc. Use 'integration' marker to exclude them
-        from normal tests
-        """
-        assert True
-
-
-@pytest.mark.parametrize(
-    "test_input,expected", [("3+5", 8), ("2+4", 6), ("3*3", 9)]
-)
-def test_with_parametrize_example(test_input, expected):
-    assert eval(test_input) == expected  # pylint: disable=eval-used
+@pytest.mark.django_db
+def test_access_to_greetings_endpoint_must_fail_on_anonymous_user(
+    anonymous_api_client,
+):
+    response = anonymous_api_client.get('/core/greetings/')
+    assert response.status_code == 401
+    assert response.json() == {
+        'detail': 'Authentication credentials were not provided.'
+    }
