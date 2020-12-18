15c4e692c164ecb56d5d8d580745c773b12707e7
tiago.paranhos <tiagoprn@gmail.com>
2020-12-18 07:30:43 -0300

    feat(wip): adding validation to the csv importer parameters.

--- personal_api/core/management/commands/import_links_from_csv.py
+++ personal_api/core/management/commands/import_links_from_csv.py
@@ -1,7 +1,12 @@
+import os
+import sys
+
 from django.contrib.auth import get_user_model
 from django.core.management.commands.test import Command as BaseCommand
 
 from core.models import Link
+
+User = get_user_model()
 
 
 class Command(BaseCommand):
@@ -12,7 +17,6 @@ class Command(BaseCommand):
     def sample_creation_handle(
         self, *args, **kwargs
     ):  # pylint: disable=unused-argument
-        User = get_user_model()
         User.objects.count()
 
         user_data = {
@@ -37,39 +41,64 @@ class Command(BaseCommand):
         parser.add_argument(
             '--username',
             dest='username',
+            required=True,
             default=None,
-            help='Specifies the username that will own imported links.',
+            help='The username that will own imported links.',
         )
         parser.add_argument(
             '--csv-file-path',
             dest='csv_file_path',
+            required=True,
             default=None,
             help=(
-                'Specifies the csv file path that contains '
-                'the links to be imported.'
+                'The full csv file path that contains links to be imported.'
             ),
         )
 
+    def validate_user(self, username: str) -> User:
+        links_user = User.objects.filter(username=username).first()
+        if not links_user:
+            available_users = User.objects.values_list(
+                'username', flat=True
+            ).order_by('username')
+            message = f'There is no use with username={username}. '
+            if len(available_users) > 0:
+                message += f'Available users: {", ".join(available_users)}'
+            else:
+                message += (
+                    'There are no available users, '
+                    'at least one must be created manually.'
+                )
+            self.stdout.write(self.style.ERROR(message))
+            sys.exit(1)
+        return links_user
+
+    def finish_if_csv_file_does_not_exist(self, path: str) -> bool:
+        if not os.path.exists(path):
+            message = f'There is not a file at filesystem path "{path}".'
+            self.stdout.write(self.style.ERROR(message))
+            sys.exit(1)
+
     def handle(self, *args, **options):  # pylint: disable=unused-argument
         """
-        Call the command with:
+        To test manually:
+
+        create-admin-superuser-without-input username=admin1 \
+            password=12345678 email=admin1@gmail.com;
 
         python personal_api/manage.py import_links_from_csv \
-            --username=tiago --csv-file-path=/tmp/links.csv
+            --username=admin1 --csv-file-path=/tmp/links.csv
 
         """
         username = options.get('username')
         csv_file_path = options.get('csv_file_path')
 
-        # TODO: validate if the user was informed,
-        #       and there is a user with the username
-
-        # TODO: validate if the csv_file_path was informed,
-        #       and the file exists on the filesystem
+        links_user = self.validate_user(username)
+        self.finish_if_csv_file_does_not_exist(csv_file_path)
 
         self.stdout.write(
             f'Importing csv file "{csv_file_path}" '
-            f'for username "{username}"... '
+            f'for username "{links_user.username}"... '
         )
 
         # TODO: continue importing from here using pandas
